4adc3e0c73c20cba29b6e4a739933f3042f370a0
Ulises Gomez <111705610+TlaloCode@users.noreply.github.com>
2023-05-07 12:24:10 -0600

    prueba2

--- html/index.php
+++ html/index.php
@@ -10,6 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>REGISTRO DE USUARIO A LA TIENDA</title>
 </head>
 
