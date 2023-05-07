89feabf55234ce8767c8c97fa02500efed8dab4b
Ulises Gomez <111705610+TlaloCode@users.noreply.github.com>
2023-05-07 19:31:20 +0000

    f

--- html/index.php
+++ html/index.php
@@ -28,9 +28,9 @@
                 </div>
                 <div class="form-signin w-100 m-auto">
                 <p class="text-center">¿Ya tienes una cuenta? Inicia sesión aquí </p>
-                <button class="w-50 btn btn-lg btn-primary mb1 bg-black" type="submit">Iniciar sesión</button>
+                <button class="w-50 btn btn-lg btn-primary mb1 bg-black" type="button">Iniciar sesión</button>
                 <p class="mt-5 mb-3 text-body-secondary"></p>
-</div>
+            </div>
             </div>
         </div>
         <main class="form-signin w-100 m-auto">
