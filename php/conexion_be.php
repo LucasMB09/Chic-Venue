1ce298827c32753eb4e38258bd72283c75c85c04
DamianOsG <88914819+DamianOsG@users.noreply.github.com>
2023-05-07 16:24:52 -0600

    Verificacion de conexion php

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,3 +1,10 @@
-<?
-    $conexion = mysql
+<?php
+
+    $conexion = mysqli_connect("localhost","root","","c_v_user");
+
+    if($conexion){
+        echo 'Conectado exitosamente a la Base de Datos';
+    }else{
+        echo 'No se ha podido conectar a la Base de Datos';
+    }
 ?>
