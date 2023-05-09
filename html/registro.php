f36d6c089938435e6aa770e31f6e06f763927eb7
DamianOsG <88914819+DamianOsG@users.noreply.github.com>
2023-05-08 19:06:00 -0600

    conectar el formulario a registro php

---
+++ html/registro.php
@@ -0,0 +1,12 @@
+<?php
+
+    $conexion = mysqli_connect("localhost","root","","c_v_user");
+
+    if($conexion){
+        echo 'Conectado exitosamente a la Base de Datos';
+    }else{
+        echo 'No se ha podido conectar a la Base de Datos';
+    }
+
+    
+?>
