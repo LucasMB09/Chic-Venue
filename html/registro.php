2440f46ed71de9ced20505654fed571c5ddf76d6
DamianOsG <88914819+DamianOsG@users.noreply.github.com>
2023-05-08 19:46:40 -0600

    comprobacion de conexion

--- html/registro.php
+++ html/registro.php
@@ -1,13 +1,8 @@
 <?php
+    include("con_db.php");
 
-    $conexion = mysqli_connect("localhost","root","","c_v_user");
-
-    if($conexion){
-        echo 'Conectado exitosamente a la Base de Datos';
-    }else{
-        echo 'No se ha podido conectar a la Base de Datos';
+    if($conex){
+        echo "Todo correcto";
     }
-
-    
 
 ?>
