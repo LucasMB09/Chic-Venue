59e329f785490bd32cfc3397435be363bfb393d3
Sam <samuel_salas@outlook.com>
2023-05-25 19:45:00 -0600

    Conexion de base de datos para mostrar articulos

---
+++ php/CreateDb.php
@@ -0,0 +1,52 @@
+<?php
+
+
+class CreateDb
+{
+        public $servername;
+        public $username;
+        public $password;
+        public $dbname;
+        public $tablename;
+        public $con;
+
+
+        // class constructor
+    public function __construct(
+        $dbname = "chicvenue",
+        $tablename = "articulo",
+        $servername = "localhost",
+        $username = "root",
+        $password = ""
+    )
+    {
+      $this->dbname = $dbname;
+      $this->tablename = $tablename;
+      $this->servername = $servername;
+      $this->username = $username;
+      $this->password = $password;
+
+      // create connection
+        $this->con = mysqli_connect($servername, $username, $password,$dbname);
+
+        // Check connection
+        if (!$this->con){
+            die("Connection failed : " . mysqli_connect_error());
+        }
+    }
+
+    // get product from the database
+    public function getData(){
+        $sql = "SELECT * FROM $this->tablename";
+        $result = mysqli_query($this->con, $sql);
+        if(mysqli_num_rows($result) > 0){
+            return $result;
+        }
+    }
+}
+
+
+
+
+
+
--- products.php
+++ products.php
@@ -1,3 +1,15 @@
+<?php
+  
+
+ 
+  require_once ('./php/CreateDb.php');
+  require_once ('./php/component.php');
+  $database = new CreateDb("chicvenue","articulo");
+
+
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -25,8 +37,8 @@
     <!-- MENU DE NAVEGACION -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="/html/index.html">
-          <img src="/assets/logo.jpg" class="rounded mx-auto d-block"  alt="" width="82" height="70">
+        <a class="navbar-brand" href="index.html">
+          <img src="assets/logo.jpg" class="rounded mx-auto d-block"  alt="" width="82" height="70">
         </a>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -62,26 +74,26 @@
             <li class="nav-item">
               &nbsp;&nbsp;&nbsp;&nbsp;
                <a class="navbar-brand" href="#">
-               <img src="/assets/filtro.png" alt="carrito" width="30" height="30" class="d-inline-block align-text-top">
+               <img src="assets/filtro.png" alt="carrito" width="30" height="30" class="d-inline-block align-text-top">
               </a>
             </li>
             <li class="nav-item">
              <li class="nav-item">
               &nbsp;&nbsp;&nbsp;&nbsp;
-               <a class="navbar-brand" href="/html/log-in.html">
-               <img src="/assets/usuario.png" alt="carrito" width="30" height="30" class="d-inline-block align-text-top">
+               <a class="navbar-brand" href="log-in.html">
+               <img src="assets/usuario.png" alt="carrito" width="30" height="30" class="d-inline-block align-text-top">
               </a>
             </li>
             <li class="nav-item">
               &nbsp;&nbsp;&nbsp;&nbsp;
                <a class="navbar-brand" href="#">
-               <img src="/assets/favoritos.JPG" alt="carrito" width="30" height="30" class="d-inline-block align-text-top">
+               <img src="assets/favoritos.JPG" alt="carrito" width="30" height="30" class="d-inline-block align-text-top">
               </a>
             </li>
             <li class="nav-item">
               &nbsp;&nbsp;&nbsp;&nbsp;
               <a class="navbar-brand" href="#">
-              <img src="/assets/carrito.png" alt="carrito" width="30" height="30" class="d-inline-block align-text-top">
+              <img src="assets/carrito.png" alt="carrito" width="30" height="30" class="d-inline-block align-text-top">
              </a>
             </li>
           </ul>
@@ -89,6 +101,18 @@
         </div>
       </div>
     </nav>
+
+    <div class="container">
+        <div class="row text-center py-5">
+            <?php
+                $result = $database->getData();
+                while ($row = mysqli_fetch_assoc($result)){
+                    component($row['id_articulo'], $row['precio'], $row['nombre_articulo'], $row['descripcion']);
+                }
+            ?>
+        </div>
+    </div>
+
 
     
     <!-- FIN MENU DE NAVEGACIÓN -->
