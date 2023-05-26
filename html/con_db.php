d72cf3ead216fff05c49e8e37e045a2bcd26eec1
Uriel Vargas <cochs7534@gmail.com>
2023-05-25 18:22:07 -0600

    commit Yoshi

--- html/con_db.php
+++ html/con_db.php
@@ -1,3 +1,81 @@
 <?php
-     $conex = mysqli_connect("localhost","root","","c_v_user");
+     session_start();
+
+     $email = $_POST["iemail"];
+     $pass = $_POST["ipass"];
+
+     $mail = " ";
+     $pas = " ";
+     $conmail = 0;
+     $con = 0;
+     // Conexión a la base de datos
+     $conexion = mysqli_connect("localhost", "root", "", "chic_venue");
+
+     // Consulta SQL para obtener los valores de una columna
+     $query = "SELECT correo_electronico FROM cliente";
+     
+     // Ejecutar consulta
+     $resultado = mysqli_query($conexion, $query);
+
+     // Verificar si la consulta fue exitosa
+     if ($resultado) {
+          // Obtener los valores de la columna
+          while ($fila = mysqli_fetch_assoc($resultado)) {
+               $valor = $fila['correo_electronico'];
+               // Hacer algo con el valor obtenido
+               //echo $valor."<br>";
+               if($valor == $email){
+                    $mail = $email;
+                    $conmail = 1;
+                    $query2 = "SELECT contraseña FROM cliente WHERE correo_electronico = '$valor'";
+                    $resultado2 = mysqli_query($conexion, $query2);
+                    /*while($fila2 = mysqli_fetch_assoc($resultado2)){
+                         $valor2 = $fila2["contraseña"];*/
+                         $fila2 = mysqli_fetch_array($resultado2);
+                         $valor2 = $fila2[0];
+                         //echo $valor2."<br>";
+                         if($valor2 == $pass){
+                              $pas = $pass;
+                              $con = 1;
+                              //break;
+                         }
+                         else{
+                              $pas = "Incorrecta";
+                              $con = 0;
+                         }
+                    //}
+
+                    if($con == 1 or $conmail == 1){
+                         break;
+                    }
+               }
+               else{
+                    $mail = "No hay";
+                    $pas = "Unknown";
+               }
+          }
+     } else {
+          // Manejar el caso de consulta fallida
+          echo "Error en la consulta: " . mysqli_error($conexion);
+     }
+
+     if($mail != "No hay" ){
+          if($pas != "Incorrecta"){
+               echo "La cuenta existe"."<br>";
+               echo "Correo: $mail"."<br>";
+               echo "Contraseña: $pas"."<br>";
+          }
+          else{
+               echo "La contraseña no es correcta"."<br>";
+          }
+     }
+     else{
+          echo "No existe ninguna cuenta asociada a ese correo"."<br>";
+     }
+
+     // Liberar recursos y cerrar conexión
+     mysqli_free_result($resultado);
+     mysqli_close($conexion);
+     
+
 ?>
---
+++ html/registro_verificar.js
@@ -0,0 +1,67 @@
+const formulario = document.getElementById("login");
+const inputs = document.querySelectorAll("#login input");
+
+const exp = {
+    email: /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/,
+    pass: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[a-zA-Z\d!@#$%^&*()]{8,}$/
+}
+
+const campos = {
+    email: false,
+    pass: false
+}
+
+const validarFormulario = (e) => {
+    switch (e.target.name) {
+        case "iemail":
+            validarCampo(exp.email, e.target,"email");
+        break;
+        case "ipass":
+            validarCampo(exp.pass, e.target,"pass");
+        break;
+    }
+}
+
+const validarCampo = (expresion, input, campo) => {
+    if(expresion.test(input.value)){
+        document.getElementById(`${campo}`).classList.remove('formulario_grupo-incorrecto');
+        document.getElementById(`${campo}`).classList.add('formulario_grupo-correcto');
+		document.querySelector(`#${campo} i`).classList.add('fa-check-circle');
+		document.querySelector(`#${campo} i`).classList.remove('fa-times-circle');
+        campos[campo] = true;
+        console.log("B")
+    } else{
+        document.getElementById(`${campo}`).classList.add('formulario_grupo-incorrecto');
+        document.getElementById(`${campo}`).classList.remove('formulario_grupo-correcto');
+		document.querySelector(`#${campo} i`).classList.add('fa-times-circle');
+		document.querySelector(`#${campo} i`).classList.remove('fa-check-circle');
+        campos[campo] = false;
+        console.log("M")
+    }
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarFormulario);
+	input.addEventListener('blur', validarFormulario);
+});
+
+formulario.addEventListener('submit', (e) => {
+	if(campos.email && campos.pass){
+	    document.getElementById('formulario_mensaje-exito').classList.add('formulario_mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('formulario_mensaje-exito').classList.remove('formulario_mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('.formulario_grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario_grupo-correcto');
+		});
+	    console.log("Llenado");
+
+	}
+    
+    else{
+        e.preventDefault();
+		document.getElementById('formulario_mensaje').classList.add('formulario_mensaje-activo');
+		console.log("llena campos");
+    }
+});
