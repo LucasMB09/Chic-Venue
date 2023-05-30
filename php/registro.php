e9ce4a5fc9cfe74378f48b5287b6d6fc7984e1e9
Uriel Vargas <99446420+Yoshi7534@users.noreply.github.com>
2023-05-29 20:42:36 -0600

    si

--- php/registro.php
+++ php/registro.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     $conexion = mysqli_connect("localhost", "root", "", "chic_venue");
     $mail = "";
@@ -55,58 +54,4 @@
         }
     }
 
-=======
-<?php
-    $conexion = mysqli_connect("localhost", "root", "", "chic_venue");
-    $mail = "";
-    $conmail = 0;
-
-    if(isset($_POST['registro'])){
-        if(strlen($_POST['nombre']) >= 1 && strlen($_POST['apellido']) >= 1 && strlen($_POST['email']) >= 1 && strlen($_POST['contrasena']) >= 1 ){
-            $nombre = trim($_POST['nombre']);
-            $apellido = trim($_POST['apellido']);
-            $email = trim($_POST['email']);
-            $contrasena = trim($_POST['contrasena']);
-            $query = "SELECT correo_electronico FROM cliente";
-            $resul = mysqli_query($conexion,$query);
-            if($resul){
-                while ($fila = mysqli_fetch_assoc($resul)) {
-                    $valor = $fila['correo_electronico'];
-                    if($valor == $email ){
-                        $mail = $email;
-                        $conmail = 1;
-                        break;
-                    }
-                    else{
-                        $mail = "No hay";
-                    }
-                }
-            }
-            
-            if($conmail == 1){
-                ?>
-                <h3 class="bad">¡Este correo ya esta asociado a una cuenta!</h3>
-                <?php
-                return;
-            }
-
-            $consulta = "INSERT INTO cliente(nombre,apellido,correo_electronico,contraseña) VALUES ('$nombre','$apellido','$email','$contrasena')";
-            $resultado = mysqli_query($conexion,$consulta);
-            if($resultado){
-                ?>
-                <h3 class="ok">¡Te has registrado correctamente!</h3>
-                <?php
-            } else {
-                ?>
-                <h3 class="bad">¡Ups ha ocurrido un error!</h3>
-                <?php
-            } 
-        } else {
-            ?>
-            <h3 class="bad">¡Por favor completa los campos!</h3>
-            <?php
-        }
-    }
-
->>>>>>> origin/main
 ?>
